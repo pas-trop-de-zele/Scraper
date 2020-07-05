3f2219fba755ab046292ab4f085df34730c8cdb0
Sam <huy.le09@yahoo.com>
2020-07-05 15:21:19 -0700

    Improve scrape function
    Add other css classes to find div

--- scraper.py
+++ scraper.py
@@ -1,34 +1,47 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 chrome_options = webdriver.ChromeOptions()
+
 """
+Preferences for efficiency
+2 - block, 1 - allow, 0 - default
+
 ***Need to further read about chrome profile and these options
 =>>>> These were forked from Dinuduke github
-
-Add some experimental options to increase efficiency
-2 - block, 1 - allow, 0 - default
 """
-prefs = {"profile.managed_default_content_settings.images":2,
+PREFS = {"profile.managed_default_content_settings.images":2,
          "profile.default_content_setting_values.notifications":2,
          "profile.managed_default_content_settings.stylesheets":2,
          "profile.managed_default_content_settings.cookies":2,
          "profile.managed_default_content_settings.javascript":1,
-         "profile.managed_default_content_settings.plugins":1,
+         "profile.managed_default_content_settings.plugins":2,
          "profile.managed_default_content_settings.popups":2,
          "profile.managed_default_content_settings.geolocation":2,
          "profile.managed_default_content_settings.media_stream":2,
          }
-chrome_options.add_experimental_option('prefs', prefs)
+
+# Potential classes name that Nike use for their size chart
+POTENTIAL_CLASSES = ['css-1uentg', 'css-1gxjmmq']
+
+# Add reference defined above
+chrome_options.add_experimental_option('prefs', PREFS)
 
 browser = webdriver.Chrome(executable_path='C:\Program Files (x86)\Google\Chrome\chromedriver.exe', options=chrome_options)
 
-browser.get("https://www.nike.com/t/lebron-17-basketball-shoe-6LSXgh/BQ3177-601")
+browser.get("https://www.nike.com/t/kyrie-6-n7-basketball-shoe-n61njg/CW1785-200")
 
-# find all divs that contains sizes
-sizes = browser.find_elements_by_class_name('css-1gxjmmq')
+# find all divs with size's class name
+for POTENTIAL_CLASS in POTENTIAL_CLASSES:
+    """
+    Look for divs with this classname
+    """
+    sizes = browser.find_elements_by_css_selector('div.' + POTENTIAL_CLASS)
+    if sizes:
+        break
 
 """
-Look inside found divs in put to check for disabled class 
+Look inside found div
 which are sizes that out of stock
 """
 for size in sizes:
