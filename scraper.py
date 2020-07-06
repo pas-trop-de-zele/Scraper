2bddb0d289b66655fd4ec049fcf967189c47242b
Sam <huy.le09@yahoo.com>
2020-07-06 00:00:59 -0700

    Select size and Add to cart User could choose their desired size Add to cart automatically

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+import time
 
 chrome_options = webdriver.ChromeOptions()
 
@@ -13,7 +13,7 @@ Preferences for efficiency
 PREFS = {"profile.managed_default_content_settings.images":2,
          "profile.default_content_setting_values.notifications":2,
          "profile.managed_default_content_settings.stylesheets":2,
-         "profile.managed_default_content_settings.cookies":2,
+         "profile.managed_default_content_settings.cookies":1,
          "profile.managed_default_content_settings.javascript":1,
          "profile.managed_default_content_settings.plugins":2,
          "profile.managed_default_content_settings.popups":2,
@@ -40,6 +40,11 @@ for POTENTIAL_CLASS in POTENTIAL_CLASSES:
     if sizes:
         break
 
+print("Available sizes:")
+
+# Dict to hash size with number choice
+size_options = {}
+choice_number = 1
 """
 Look inside found div
 which are sizes that out of stock
@@ -49,6 +54,32 @@ for size in sizes:
     # if attribute 'disabled' exist means out out stock
     out_of_stock = size_display.get_attribute('disabled')
     if out_of_stock:
-        print(f"{size.text} <= OUT OF STOCK")
+        print(f"{choice_number}. {size.text} <= OUT OF STOCK")
     else:
-        print(f"{size.text}")
+        print(f"{choice_number}. {size.text}")
+
+    """
+    save 'choice_number' : [size text, size display object] 
+
+    size display object to perform actions later
+    """ 
+    size_options[choice_number] = [size.text, size_display]
+    choice_number += 1
+
+# Get size from user
+number_option = int(input("Please select number according to your desired size: "))
+# Get size button element saved from above
+size_button = size_options[number_option][1]
+# Free up space
+size_options = {}
+
+add_to_cart = browser.find_element_by_css_selector("button[aria-label='Add to Cart']")
+mouse = webdriver.ActionChains(browser)
+# Add item to cart
+mouse.click(size_button).click(add_to_cart).perform()
+# Go to cart
+browser.get("https://www.nike.com/us/en/cart")
+
+# NEED FIX STALEELEMENTEXCEPTION==============================
+# mouse.click(browser.find_element_by_css_selector("button[data-automation='go-to-checkout-button']")).perform()
+
