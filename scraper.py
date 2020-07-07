80ddf19af36ffe748bdc55f402101ac3b7484eb4
Sam <huy.le09@yahoo.com>
2020-07-07 07:56:07 -0700

    Able to fill out forms Able to fill out form up to City, need to find out how to deal with drop downs

--- scraper.py
+++ scraper.py
@@ -86,7 +86,7 @@ size_options = {}
 add_to_cart = browser.find_element_by_css_selector("button[aria-label='Add to Cart']")
 mouse = ActionChains(browser)
 # Add item to cart
-mouse.click(size_button).pause(0.5).click(add_to_cart).pause(0.5).perform()
+mouse.click(size_button).pause(0.5).click(add_to_cart).pause(1).perform()
 # Go to cart
 browser.get("https://www.nike.com/us/en/cart")
 
@@ -97,14 +97,30 @@ check_out = browser.find_element_by_css_selector("button[data-automation='go-to-
 guest_checkout = browser.find_element_by_css_selector("button[data-automation='guest-checkout-button']")
 mouse.click(check_out).pause(2).click(guest_checkout).perform()
 
-# wait until 'firstname field' element is available on checkout page
+# wait until 'Enter address manually' option is available on checkout page
 element = WebDriverWait(browser, 10).until(
-    expected_conditions.presence_of_element_located((By.ID, "firstName"))
+    expected_conditions.presence_of_element_located((By.ID, "addressSuggestionOptOut"))
 )
 
 # FILLING IN ADDRESS FORMS
 mouse = ActionChains(browser)
+# Select enter address manually
+browser.find_element_by_id('addressSuggestionOptOut').click()
+# Select to add address field 2
+browser.find_element_by_css_selector("button[aria-controls='address2']").click()
+# Find according fields to enter information
 first_name = browser.find_element_by_id("firstName")
 last_name = browser.find_element_by_id("lastName")
+address1 = browser.find_element_by_id("address1")
+address2 = browser.find_element_by_id("address2")
+city = browser.find_element_by_id("city")
+
+
+mouse.send_keys_to_element(first_name, 'Pablo')
+mouse.send_keys_to_element(last_name, 'Escobar')
+mouse.send_keys_to_element(address1, '9039 Bolsa Ave')
+mouse.send_keys_to_element(address2, 'Suite 309')
+mouse.send_keys_to_element(city, 'Westminster')
+mouse.perform()
 
 
