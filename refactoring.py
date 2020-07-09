a1f5e20940473aec124a618b4fab21cd43f5b7a9
Sam <huy.le09@yahoo.com>
2020-07-09 07:59:33 -0700

    Refactoring scraper script into OOP

---
+++ refactoring.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import Select
+
+class Scraper:
+    
