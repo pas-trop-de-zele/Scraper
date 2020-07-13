8f2ba8750e9334123f625173907a196f6168e92f
pas-trop-de-zele <67793141+pas-trop-de-zele@users.noreply.github.com>
2020-07-13 11:44:42 -0700

    Delete refactoring.py

--- refactoring.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.ui import Select
-
-class Scraper:
-    
